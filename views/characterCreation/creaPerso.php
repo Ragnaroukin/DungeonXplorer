368e6169c2aba565f2d63e249f8d269035f96970
Maël Lecapitaine <mael.lecapitaine@gmail.com>
2025-11-21 11:54:16 +0100

    Amélioration CSS + début formulaire création

--- views/characterCreation/creaPerso.php
+++ views/characterCreation/creaPerso.php
@@ -10,23 +10,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Création de Personnage</title>
-    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="../../styles/styleCreaPerso.css">
 </head>
 <body class="classSelectBackground">
     <?php ?>
     <div class="classCards">
         <div class="card">
-            <img src="../img/BarbarianClass.png" alt="The image of a Barbarian">
-            <button type="button" class="classSelectButton">Barbare</button>
+            <img src="../../img/BarbarianClass.png" alt="The image of a Barbarian">
+            <form action="createBarbarian.php" method="post">
+                <button type="button" class="classSelectButton">Barbare</button>
+            </form>
         </div>
 
         <div class="card">
-            <img src="../img/WizardClass.png" alt="The image of a Barbarian">
+            <img src="../../img/WizardClass.png" alt="The image of a Barbarian">
             <button type="button" class="classSelectButton">Mage</button>
         </div>
 
         <div class="card">
-            <img src="../img/RogueClass.png" alt="The image of a Barbarian">
+            <img src="../../img/RogueClass.png" alt="The image of a Barbarian">
             <button type="button" class="classSelectButton">Voleur</button>
         </div>
     </div>
---
+++ views/characterCreation/createHero.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Création de Personnage</title>
+</head>
+<body>
+    <form action="" method="post">
+        Nom : <input type="text">
+    </form>
+</body>
+</html>
+
+<?php
+
+function createHero(String $heroType) {
+    if ($heroType == "Barbarian") {
+
+    }
+}
+
+?>
