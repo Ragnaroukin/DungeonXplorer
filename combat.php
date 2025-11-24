df512303f26b90f95ae35bb9c7d5bbccebdcf3e9
BOUIN Yohan <bouin.yohan@outlook.fr>
2025-11-24 16:54:26 +0100

    affichage hero

--- combat.php
+++ combat.php
@@ -3,10 +3,10 @@ require_once("views/header.php");
 ?>
 
 <?php
-    $reponseMonstre = $bdd->prepare('SELECT monster_nom,monster_image,monster_pv,monster_mana,monster_initiative,monster_strength,monster_attack,monster_xp FROM Monster where monster_id = ?');
+    $reponseMonstre = $bdd->prepare('SELECT monster_name,monster_image,monster_pv,monster_mana,monster_initiative,monster_strength,monster_attack,monster_xp FROM Monster where monster_id = ?');
     $reponseMonstre->execute(array($_POST['monster_id']));
 
-    $reponseHero = $bdd->prepare('SELECT hero_name ,class_id,hero_pv ,hero_mana ,hero_strength ,hero_initiative ,hero_level ,hero_xp  FROM hero where hero_id = ?');
+    $reponseHero = $bdd->prepare('SELECT hero_name ,class_id,hero_image,hero_pv ,hero_mana ,hero_strength ,hero_initiative ,hero_level ,hero_xp  FROM hero where hero_id = ?');
     $reponseHero->execute(array($_POST['hero_id']));
 
     $reponseClass = $bdd->prepare('SELECT class_name  ,class_base_pv ,class_base_mana  ,class_base_strength  ,class_base_initiative  FROM Class where class_id  = ?');
@@ -17,8 +17,21 @@ require_once("views/header.php");
 ?>
 
 <img src= "<?php  $reponseMonstre['monstre_image']  ?>" alt="image_monstre">
-<h3><?php  $reponseMonstre['monstre_nom'] ?></h3>
-<p><?php  $reponseMonstre['monster_pv'] ?> PV / <?php  $reponseMonstre['monster_mana'] ?> Mana / <?php  $reponseMonstre['monster_strength'] ?> Force </p>
+<h3><?php  $reponseMonstre['monstre_name'] ?></h3>
+<p> <?php  $reponseMonstre['monster_pv'] ?> PV |
+    <?php  $reponseMonstre['monster_mana'] ?> Mana |
+    <?php  $reponseMonstre['monster_strength'] ?> Force 
+</p>
+
+<img src= "<?php $reponseHero['hero_image']  ?>" alt="image_hero">
+<h3><?php $reponseHero['hero_name'] ?></h3>
+<p> 
+    <?php $reponseHero['hero_pv'] ?> / <?php $reponseClass['class_base_pv'] + $reponseLevel['level_pv_bonus'] ?> PV |
+    <?php $reponseHero['hero_mana'] ?> / <?php $reponseClass['class_base_mana'] + $reponseLevel['level_mana_bonus'] ?> Mana |
+    <?php $reponseHero['hero_initiative'] ?> / <?php $reponseClass['class_base_initiative'] + $reponseLevel['level_initiative_bonus'] ?> Initiative |
+    <?php $reponseHero['hero_strength'] ?> / <?php $reponseClass['class_base_strength'] + $reponseLevel['level_strength_bonus'] ?> Force |
+    Level : <?php $reponseHero['hero_level'] ?> (<?php $reponseHero['hero_xp'] ?>/<?php $reponseLevel['level_required_xp']?> <!-- Xp requis n'est pas forcement celui du suivant ! revoir quand la base est bien définis -->)
+</p> 
 
 <form action="" method="post"> 
     <input type="radio" name="choice" value="physical"> Attaque physique
