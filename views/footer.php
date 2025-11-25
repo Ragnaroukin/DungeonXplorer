7614b64bb5ec3d58d573dcfff731674c69a80014
Arthur POIRIER--DUBOIS <arthurpoidub@gmail.com>
2025-11-25 01:24:29 +0100

    Correction .htaccess

--- views/footer.php
+++ views/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer py-4">
     <div class="container">
         <div class="row align-items-center">
-            <div class="col-lg-4 text-lg-start">Copyright &copy; Your Website 2023</div>
+            <div class="col-lg-4 text-lg-start">Copyright &copy; DungeonXplorer</div>
             <div class="col-lg-4 my-3 my-lg-0">
                 <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Twitter"><i
                         class="fab fa-twitter"></i></a>
@@ -22,7 +22,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 
 <!-- Core theme JS-->
-<script src="/DungeonXplorer/js/scripts.js"></script>
+<script src="js/scripts.js"></script>
 </body>
 
 </html>
